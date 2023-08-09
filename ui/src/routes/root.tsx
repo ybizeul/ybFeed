b96c759bf58907c17f5a53dc86a6411814c11c1e
Yann Bizeul <yann@tynsoe.org>
2023-08-09 19:08:05 +0200

    new: Small redesign of home page

--- ui/src/routes/root.tsx
+++ ui/src/routes/root.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router';
-import { Form, Input } from 'antd';
+import { Form, Input, Col } from 'antd';
 import { useState } from "react"
 import YBBreadCrumb from '../YBBreadCrumb'
 
@@ -16,27 +16,32 @@ export default function Root() {
     if (window.location.pathname !== "/") {
         setGoToPath(window.location.pathname)
     }
-
+  
     return (
         <>
         {goToPath && (
             <Navigate to={goToPath} />
         )}
         <YBBreadCrumb />
+        <div className='center'>
         <p><b>Welcome to ybFeed</b></p>
         <p>
             Choose a unique name for your feed :
         </p>
-        <Form name="basic" layout="inline" onFinish={handleFinish}>
+        <Form name="basic" className="form-container" onFinish={handleFinish}>
+            <center>
             <Form.Item
                 name="feed"
             >
                 <Input
+                    className="input-field"
                     placeholder="Feed name"
                     onChange={(e) => setFeed(e.currentTarget.value)}
                 />
             </Form.Item>
+            </center>
         </Form>
+        </div>
         </>
     )
 }
