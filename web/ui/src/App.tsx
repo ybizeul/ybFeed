f82123baab52f400c240f1ad4e0814afa1ea542d
Yann Bizeul <yann@tynsoe.org>
2023-08-22 21:55:37 +0200

    removed logging

--- web/ui/src/App.tsx
+++ web/ui/src/App.tsx
@@ -29,7 +29,6 @@ const App: React.FC = () => {
   const windowQuery = window.matchMedia("(prefers-color-scheme:dark)");
 
   const darkModeChange = useCallback((event: MediaQueryListEvent) => {
-    console.log(event.matches ? true : false);
     setDarkMode(event.matches ? true : false);
   }, []);
 
@@ -41,7 +40,6 @@ const App: React.FC = () => {
   }, [windowQuery, darkModeChange]);
 
   useEffect(() => {
-    console.log(windowQuery.matches ? true : false);
     setDarkMode(windowQuery.matches ? true : false);
     // eslint-disable-line react-hooks/exhaustive-deps
   }, [windowQuery.matches]);
