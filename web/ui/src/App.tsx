4eabfec5f4b549a9e918ecf5f8d9f2a73d9aac63
Yann Bizeul <yann@tynsoe.org>
2023-08-30 11:46:01 +0200

    formatting

--- web/ui/src/App.tsx
+++ web/ui/src/App.tsx
@@ -14,71 +14,74 @@ import Root from "./routes/root";
 import { FeedComponent } from "./YBFeed";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/:feed",
-    element: <FeedComponent />,
-  },
+    {
+        path: "/",
+        element: <Root />,
+    },
+    {
+        path: "/:feed",
+        element: <FeedComponent />,
+    },
 ]);
 
 const App: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const [version, setVersion] = useState("");
-  const windowQuery = window.matchMedia("(prefers-color-scheme:dark)");
+    const [darkMode, setDarkMode] = useState(false);
+    const [version, setVersion] = useState("");
+    const windowQuery = window.matchMedia("(prefers-color-scheme:dark)");
 
-  const darkModeChange = useCallback((event: MediaQueryListEvent) => {
-    setDarkMode(event.matches ? true : false);
-  }, []);
+    const darkModeChange = useCallback((event: MediaQueryListEvent) => {
+        setDarkMode(event.matches ? true : false);
+    }, []);
 
-  useEffect(() => {
-    windowQuery.addEventListener("change", darkModeChange);
-    return () => {
-      windowQuery.removeEventListener("change", darkModeChange);
-    };
-  }, [windowQuery, darkModeChange]);
+    useEffect(() => {
+        windowQuery.addEventListener("change", darkModeChange);
+        return () => {
+            windowQuery.removeEventListener("change", darkModeChange);
+        };
+    }, [windowQuery, darkModeChange]);
 
-  useEffect(() => {
-    setDarkMode(windowQuery.matches ? true : false);
-    // eslint-disable-line react-hooks/exhaustive-deps
-  }, [windowQuery.matches]);
+    useEffect(() => {
+        setDarkMode(windowQuery.matches ? true : false);
+        // eslint-disable-line react-hooks/exhaustive-deps
+    }, [windowQuery.matches]);
 
-  useEffect(() => {
-    fetch("/api")
-    .then(r => {
-      let v = r.headers.get("Ybfeed-Version")
-      if (v !== null) {
-        setVersion(v)
-      }
+    useEffect(() => {
+        fetch("/api")
+        .then(r => {
+            let v = r.headers.get("Ybfeed-Version")
+            if (v !== null) {
+            setVersion(v)
+            }
+        })
     })
-  })
-  return (
-    <ConfigProvider
-    theme={{
-      algorithm: darkMode ? theme.darkAlgorithm : theme.compactAlgorithm
-    }}
-  >
-  <div className="App">
-    <Layout style={{minHeight:"100vh"}}>
-        <Content>
-        <Row>
-          <Col xs={1} lg={6}/>
-          <Col xs={22} lg={12}>
-            <RouterProvider router={router} />
-          </Col>
-          <Col xs={1} lg={6}/>
-        </Row>
-      </Content>
-      <Footer>
-        <Space style={{fontSize: '0.8em', opacity: '0.4', width: '100%', justifyContent: 'center'}}>
-          ybFeed {version}
-        </Space>
-      </Footer>
-    </Layout>
-  </div>
-  </ConfigProvider>
+    return (
+        <ConfigProvider
+            theme={{
+                algorithm: darkMode ? theme.darkAlgorithm : theme.compactAlgorithm
+            }}
+        >
+        <div className="App">
+            <Layout style={{minHeight:"100vh"}}>
+                <Content>
+                <Row>
+                    <Col xs={1} lg={6}/>
+                    <Col xs={22} lg={12}>
+                    <RouterProvider router={router} />
+                    </Col>
+                    <Col xs={1} lg={6}/>
+                </Row>
+                </Content>
+                <Footer>
+                <Space  style={{fontSize: '0.8em', 
+                                opacity: '0.4',
+                                width: '100%', 
+                                justifyContent: 'center'}}>
+                    ybFeed {version}
+                </Space>
+                </Footer>
+            </Layout>
+        </div>
+    </ConfigProvider>
 )};
 
 export default App;
