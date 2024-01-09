ef4aa8806a68625da2ac1deecc2796eaafff18bb
Yann Bizeul <yann@tynsoe.org>
2024-01-09 08:23:12 +0100

    Removed unnecessary logging

--- web/ui/public/service-worker.js
+++ web/ui/public/service-worker.js
@@ -1,8 +1,5 @@
 /*eslint no-restricted-globals: ["error"]*/
 self.addEventListener('push', event => {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
     const title = 'YBFeed Notification';
     const options = {
       body: event.data.text(),
