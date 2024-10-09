0f5d9aa64a58d42a25b1ff96b05c8b0fb313e9e8
Yann Bizeul <yann@tynsoe.org>
2024-10-09 13:28:06 +0200

    removed unused function

--- web/ui/src/notifications.ts
+++ web/ui/src/notifications.ts
@@ -31,11 +31,11 @@ export function Subscribe(vapid: string) {
     })
 }
 
-function urlBase64ToUint8Array(base64String: string) {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/');
-    const rawData = window.atob(base64);
-    return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
-}
+// function urlBase64ToUint8Array(base64String: string) {
+//     const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+//     const base64 = (base64String + padding)
+//         .replace(/-/g, '+')
+//         .replace(/_/g, '/');
+//     const rawData = window.atob(base64);
+//     return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+// }
